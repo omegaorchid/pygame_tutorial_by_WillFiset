1430139256d5f3dbdce5352c1242c72a56b23b07
Dameon Orin <omegaorchid@gmail.com>
2016-10-13 20:24:51 +0100

    tutorial 02 :: display game screen

--- tutorials/main.py
+++ tutorials/main.py
@@ -1,5 +1,14 @@
 import pygame
+import sys
 
 pygame.init()
 
-print("It worked")
+screen = pygame.display.set_mode((640, 360), 0, 32)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
